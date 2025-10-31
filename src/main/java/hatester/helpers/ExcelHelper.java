d36ce054fbce9d2eefe69b7b95db787a4300ee7d
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-10-31 11:36:20 +0700

    add code getDataHashTable

--- src/main/java/hatester/helpers/ExcelHelper.java
+++ src/main/java/hatester/helpers/ExcelHelper.java
@@ -8,8 +8,10 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class ExcelHelper {
@@ -199,7 +201,7 @@ public class ExcelHelper {
 
             int noOfCols = row.getLastCellNum(); //Số lượng cột trong dòng tiêu đề
 
-            LogUtils.info(noOfRows + " - " + noOfCols);
+//            LogUtils.info(noOfRows + " - " + noOfCols);
 
             Cell cell;
             data = new Object[noOfRows - 1][noOfCols];  //Mỗi dòng dữ liệu (bỏ dòng header) là 1 hàng trong data
@@ -242,5 +244,70 @@ public class ExcelHelper {
             throw new RuntimeException(e);
         }
         return data;  //Trả mảng Object[][] để DataProvider hoặc test script dùng
+    }
+
+    //Hàm này dùng cho trường hợp nhiều Field trong File Excel
+    public int getColumns() {
+        try {
+            Row row = sh.getRow(0);
+            return row.getLastCellNum();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw (e);
+        }
+    }
+
+    //Get last row number (lấy vị trí dòng cuối cùng tính từ 0)
+    public int getLastRowNum() {
+        return sh.getLastRowNum();
+    }
+
+    //Lấy số dòng có data đang sử dụng
+    public int getPhysicalNumberOfRows() {
+        return sh.getPhysicalNumberOfRows();
+    }
+
+    public Object[][] getDataHashTable(String excelPath, String sheetName, int startRow, int endRow) {
+        System.out.println("Excel Path: " + excelPath);
+        Object[][] data = null;
+
+        try {
+            File f = new File(excelPath);
+            if (!f.exists()) {
+                try {
+                    System.out.println("File Excel path not found.");
+                    throw new IOException("File Excel path not found.");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            FileInputStream fis = new FileInputStream(excelPath);
+
+            wb = new XSSFWorkbook(fis);
+
+            sh = wb.getSheet(sheetName);
+
+            int rows = getLastRowNum();
+            int columns = getColumns();
+
+            System.out.println("Row: " + rows + " - Column: " + columns);
+            System.out.println("StartRow: " + startRow + " - EndRow: " + endRow);
+
+            data = new Object[(endRow - startRow) + 1][1];
+            Hashtable<String, String> table = null;
+            for (int rowNums = startRow; rowNums <= endRow; rowNums++) {
+                table = new Hashtable<>();
+                for (int colNum = 0; colNum < columns; colNum++) {
+                    table.put(getCellData(colNum, 0), getCellData(colNum, rowNums));
+                }
+                data[rowNums - startRow][0] = table;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
     }
 }
