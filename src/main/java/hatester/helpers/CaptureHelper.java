a2826e9866956dafaedcfff6d14a29d058cfddf1
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-10-31 10:13:40 +0700

    add code screenRecorder

---
+++ src/main/java/hatester/helpers/CaptureHelper.java
@@ -0,0 +1,112 @@
+package hatester.helpers;
+
+import hatester.drivers.DriverManager;
+import hatester.utils.LogUtils;
+import org.monte.media.Format;
+import org.monte.media.FormatKeys;
+import org.monte.media.Registry;
+import org.monte.media.math.Rational;
+import org.monte.screenrecorder.ScreenRecorder;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+import static org.monte.media.FormatKeys.*;
+import static org.monte.media.VideoFormatKeys.*;
+
+public class CaptureHelper extends ScreenRecorder {
+    // Record with Monte Media library
+    public static ScreenRecorder screenRecorder;
+    public String name;
+
+    //Hàm xây dựng
+    public CaptureHelper(GraphicsConfiguration cfg, Rectangle captureArea, Format fileFormat, Format screenFormat,
+                         Format mouseFormat, Format audioFormat, File movieFolder, String name) throws IOException, AWTException {
+        super(cfg, captureArea, fileFormat, screenFormat, mouseFormat, audioFormat, movieFolder);
+        this.name = name;
+    }
+
+    //Hàm này bắt buộc để ghi đè custom lại hàm trong thư viện viết sẵn
+    @Override
+    protected File createMovieFile(Format fileFormat) throws IOException {
+        if (!movieFolder.exists()) {
+            movieFolder.mkdirs();   //Nếu movieFolder chưa tồn tại -> mkdirs() (tạo thư mục)
+        } else if (!movieFolder.isDirectory()) {   //Nếu movieFolder không phải là thư mục -> ném IOException
+            throw new IOException("\"" + movieFolder + "\" is not a directory.");
+        }
+        return new File(movieFolder, name + "_" + SystemHelper.getDatetimeNowFormat() + "."
+                + Registry.getInstance().getExtension(fileFormat));  //Tạo File mới
+    }
+
+    // Start record video
+    public static void startRecord(String videoRecordName) {
+        //Tạo thư mục để lưu file video vào
+        File file = new File(SystemHelper.getCurrentDir() + PropertiesHelper.getValue("VIDEO_RECORD_PATH"));
+        //lấy kích thước màn hình để làm kích cỡ của khung video
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+
+        Rectangle captureSize = new Rectangle(0, 0, width, height);   //quay toàn bộ màn hình
+
+        //Tùy chỉnh những thông số trong video
+        GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+        try {
+            screenRecorder = new CaptureHelper(gc, captureSize, new Format(MediaTypeKey, FormatKeys.MediaType.FILE,
+                    MimeTypeKey, MIME_AVI), new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey,
+                    ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE, CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                    DepthKey, 24, FrameRateKey, Rational.valueOf(15), QualityKey, 1.0f, KeyFrameIntervalKey, 15 * 60),
+                    new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black", FrameRateKey, Rational.valueOf(30)),
+                    null, file, videoRecordName);
+
+            screenRecorder.start();  //gọi screenRecorder.start() để bắt đầu quay
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Stop record video
+    public static void stopRecord() {
+        try {
+            if (screenRecorder != null) {
+                screenRecorder.stop();
+                screenRecorder = null;
+                LogUtils.info("Video recording stopped successfully.");
+            } else {
+                LogUtils.info("screenRecorder is null — skipping stopRecord.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void takeScreenshot(String screenshotName) {
+        // Tạo tham chiếu của TakesScreenshot
+        TakesScreenshot ts = (TakesScreenshot) DriverManager.getDriver();
+
+        //getScreenshotAs: phương thức chính để chụp ảnh màn hình
+        //yêu cầu Selenium xuất kết quả screenshot ra một file tạm thời (File object)
+        File source = ts.getScreenshotAs(OutputType.FILE);
+
+        //tạo một đối tượng File đại diện cho thư mục chứa ảnh screenshot
+        File theDir = new File(PropertiesHelper.getValue("SCREENSHOT_PATH"));
+
+        // Kiểm tra folder tồn tại. Nếu không thì tạo mới folder theo đường dẫn
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
+
+        //Lưu file ảnh với tên cụ thể vào đường dẫn
+        try {
+            FileHandler.copy(source, new File(PropertiesHelper.getValue("SCREENSHOT_PATH") + "/" + screenshotName + ".png"));
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+}
