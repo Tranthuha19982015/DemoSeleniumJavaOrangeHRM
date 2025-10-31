d73c85e507a20def9bd5596d23ad9c35116ede73
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-10-31 10:07:03 +0700

    add code getDatetimeNowFormat

--- src/main/java/hatester/helpers/SystemHelper.java
+++ src/main/java/hatester/helpers/SystemHelper.java
@@ -2,6 +2,8 @@ package hatester.helpers;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class SystemHelper {
@@ -12,5 +14,17 @@ public class SystemHelper {
 
     public static String getCurrentDatetime() {
         return new SimpleDateFormat("_ddMMyyyy_HHmmss").format(new Date());
+    }
+
+    public static String getDatetimeNowFormat() {
+        //Lấy thời gian hiện tại
+        LocalDateTime now = LocalDateTime.now();
+
+        //Định dạng ngày giờ theo partern
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        //Chuyển sang chuỗi và thay thế ký tự
+        String formated = now.format(formatter).replace("-", "_").replace(" ", "_").replace(":", "_");
+        return formated;
     }
 }
