b98d69ed6e17b64c4ec9d909b8734c94aa261204
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-13 10:52:13 +0700

    update file constants để cấu hình dữ liệu chung

--- src/main/java/hatester/constants/FrameworkConstant.java
+++ src/main/java/hatester/constants/FrameworkConstant.java
@@ -7,8 +7,9 @@ public class FrameworkConstant {
     }
 
     public static String BROWSER = PropertiesHelper.getValue("BROWSER");
-    public static String WINDOW_SIZE=PropertiesHelper.getValue("WINDOW-SIZE");
+    public static String URL_RISE=PropertiesHelper.getValue("URL");
+    public static String WINDOW_SIZE = PropertiesHelper.getValue("WINDOW-SIZE");
     public static String HEADLESS = PropertiesHelper.getValue("HEADLESS");
-    public static String SCREENSHOT_PATH=PropertiesHelper.getValue("SCREENSHOT_PATH");
-    public static String VIDEO_RECORD_PATH=PropertiesHelper.getValue("VIDEO_RECORD_PATH");
+    public static String SCREENSHOT_PATH = PropertiesHelper.getValue("SCREENSHOT_PATH");
+    public static String VIDEO_RECORD_PATH = PropertiesHelper.getValue("VIDEO_RECORD_PATH");
 }
