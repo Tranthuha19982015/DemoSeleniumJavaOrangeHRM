15d38dff6b02c25516f168c056ccd14f5270f6b8
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-13 15:48:25 +0700

    add class ExtentReportManager và ExtentTestManager để hỗ trợ xuất báo cáo

---
+++ src/main/java/hatester/reports/ExtentReportManager.java
@@ -0,0 +1,17 @@
+package hatester.reports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportManager {
+    private static final ExtentReports extentReports = new ExtentReports();
+
+    public synchronized static ExtentReports getExtentReports() {
+        ExtentSparkReporter reporter = new ExtentSparkReporter("reports/extentreport/extentreport.html");
+        reporter.config().setReportName("Extent Report | Ha Tester");
+        extentReports.attachReporter(reporter);
+        extentReports.setSystemInfo("Framework Name", "Selenium Java | HA Tester");
+        extentReports.setSystemInfo("Author", "Ha Tester");
+        return extentReports;
+    }
+}
