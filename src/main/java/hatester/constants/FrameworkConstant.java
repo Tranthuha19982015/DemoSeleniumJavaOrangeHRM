22770eefe47d4b21c8a31194e72478d09dec38d4
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-13 11:51:33 +0700

    add constant EXCEL_DATA_FILE_PATH

--- src/main/java/hatester/constants/FrameworkConstant.java
+++ src/main/java/hatester/constants/FrameworkConstant.java
@@ -1,13 +1,16 @@
 package hatester.constants;
 
 import hatester.helpers.PropertiesHelper;
+import hatester.helpers.SystemHelper;
 
 public class FrameworkConstant {
     private FrameworkConstant() {
     }
 
+    public static String EXCEL_DATA_FILE_PATH = PropertiesHelper.getValue("EXCEL_DATA_FILE_PATH");
+
     public static String BROWSER = PropertiesHelper.getValue("BROWSER");
-    public static String URL_RISE=PropertiesHelper.getValue("URL");
+    public static String URL_RISE = PropertiesHelper.getValue("URL");
     public static String WINDOW_SIZE = PropertiesHelper.getValue("WINDOW-SIZE");
     public static String HEADLESS = PropertiesHelper.getValue("HEADLESS");
     public static String SCREENSHOT_PATH = PropertiesHelper.getValue("SCREENSHOT_PATH");
