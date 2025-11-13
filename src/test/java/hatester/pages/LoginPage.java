bdb15e781091390163572a96d53eea207aab80a2
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-13 11:52:49 +0700

    update code login CRM to login Rise

--- src/test/java/hatester/pages/LoginPage.java
+++ src/test/java/hatester/pages/LoginPage.java
@@ -1,73 +1,74 @@
 package hatester.pages;
 
 import hatester.common.BasePage;
+import hatester.constants.FrameworkConstant;
 import hatester.helpers.PropertiesHelper;
 import hatester.keywords.WebUI;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 
 public class LoginPage extends BasePage {
-    private By headerPage = By.xpath("//h1[normalize-space()='Login']");
-    private By inputEmailAddress = By.xpath("//input[@id='email']");
+    private By imgLoginPage = By.xpath("//div[contains(@class,'card-header')]/img");
+    private By inputEmail = By.xpath("//input[@id='email']");
     private By inputPassword = By.xpath("//input[@id='password']");
-    private By buttonLogin = By.xpath("//button[normalize-space()='Login']");
-    private By checkboxRememberMe = By.xpath("//input[@id='remember']");
-    private By linkForgotPassword = By.xpath("//a[normalize-space()='Forgot Password?']");
-    private By errorMessageInValidEmailOrPassword = By.xpath("//div[@id='alerts']/div[normalize-space()='Invalid email or password']");
-    private By errorMessageEmailRequired = By.xpath("//div[normalize-space()='The Email Address field is required.']");
-    private By errorMessagePasswordRequired = By.xpath("//div[normalize-space()='The Password field is required.']");
+    private By buttonSignIn = By.xpath("//button[normalize-space()='Sign in']");
+    private By linkForgotPassword = By.xpath("//a[normalize-space()='Forgot password?']");
+    private By linkSignUp = By.xpath("//a[normalize-space()='Sign up']");
+    private By errorMessageInValidEmailOrPassword = By.xpath("//form[@id='signin-form']/div[contains(@class,'alert-danger')]");
+    private By errorMessageEmailRequired = By.xpath("//input[@id='email']/following-sibling::span[@id='email-error']");
+    private By errorMessagePasswordRequired = By.xpath("//input[@id='password']/following-sibling::span[@id='password-error']");
 
-    public void openCRM(String url) {
-        WebUI.openURL(PropertiesHelper.getValue("URL"));
+    public void openRISE(String url) {
+        WebUI.openURL(FrameworkConstant.URL_RISE);
     }
 
-    public void verifyHeaderLoginDisplay() {
-        Assert.assertTrue(WebUI.checkElementExist(headerPage, 10), "The header page is not displayed.");
+    public void verifyImageLoginDisplay() {
+        Assert.assertTrue(WebUI.checkElementExist(imgLoginPage,5), "The img page is not displayed.");
     }
 
     public void enterEmail(String email) {
-        WebUI.setText(inputEmailAddress, email);
+        WebUI.setText(inputEmail, email);
     }
 
     public void enterPassword(String password) {
         WebUI.setText(inputPassword, password);
     }
 
-    public void clickLogin() {
-        WebUI.clickToElement(buttonLogin);
+    public void clickSignIn() {
+        WebUI.clickToElement(buttonSignIn);
     }
 
-    public void loginCRM(String email, String password) {
-        openCRM(PropertiesHelper.getValue("URL"));
-        verifyHeaderLoginDisplay();
+    public void loginRISE(String email, String password) {
+        openRISE(FrameworkConstant.URL_RISE);
+        verifyImageLoginDisplay();
         enterEmail(email);
         enterPassword(password);
-        clickLogin();
+        clickSignIn();
         WebUI.waitForPageLoaded();
     }
 
-    public void loginCRM() {
-        openCRM(PropertiesHelper.getValue("URL"));
-        verifyHeaderLoginDisplay();
+    public void loginRISE() {
+        openRISE(FrameworkConstant.URL_RISE);
+        verifyImageLoginDisplay();
         enterEmail(PropertiesHelper.getValue("EMAIL_ADDRESS"));
         enterPassword(PropertiesHelper.getValue("PASSWORD"));
-        clickLogin();
+        clickSignIn();
         verifyLoginSuccess();
     }
 
     public void verifyLoginSuccess() {
-        Assert.assertTrue(WebUI.checkElementExist(menuDashboard,5), "The Dashboard menu is not displayed.");
+        Assert.assertTrue(WebUI.checkElementExist(menuDashboard, 5), "The Dashboard menu is not displayed.");
     }
 
-    public void verifyLoginFailedWithEmailOrPasswordInvalid(){
-        Assert.assertTrue(WebUI.checkElementExist(errorMessageInValidEmailOrPassword,5),"The error message for invalid input is not displayed.");
+    public void verifyLoginFailedWithEmailOrPasswordInvalid() {
+        Assert.assertTrue(WebUI.checkElementExist(errorMessageInValidEmailOrPassword, 5), "The error message for invalid input is not displayed.");
     }
 
-    public void verifyLoginFailedWithEmailIsNull(){
-        Assert.assertTrue(WebUI.checkElementExist(errorMessageEmailRequired,5),"The error message for a required email is not displayed.");
+    public void verifyLoginFailedWithEmailRequire() {
+        Assert.assertTrue(WebUI.checkElementExist(errorMessageEmailRequired, 5), "The error message for a required email is not displayed.");
     }
 
-    public void verifyLoginFailedWithPasswordIsNull(){
-        Assert.assertTrue(WebUI.checkElementExist(errorMessagePasswordRequired,5),"The error message for a required password is not displayed.");
+    public void verifyLoginFailedWithPasswordRequire() {
+        Assert.assertTrue(WebUI.checkElementExist(errorMessagePasswordRequired, 5), "The error message for a required password is not displayed.");
     }
 }
--- src/test/java/hatester/testcases/LoginTest.java
+++ src/test/java/hatester/testcases/LoginTest.java
@@ -2,7 +2,6 @@ package hatester.testcases;
 
 import hatester.common.BaseTest;
 import hatester.common.DataProviderFactory;
-import hatester.helpers.PropertiesHelper;
 import hatester.pages.LoginPage;
 import org.testng.annotations.Test;
 
@@ -12,23 +11,37 @@ public class LoginTest extends BaseTest {
     private LoginPage loginPage;
 
     @Test(priority = 1, dataProvider = "data_login_excel_success", dataProviderClass = DataProviderFactory.class)
-    public void testLoginSuccess(Hashtable< String, String > data) {
+    public void testLoginSuccess(Hashtable<String, String> data) {
         loginPage = new LoginPage();
-        loginPage.loginCRM(data.get("EMAIL"), data.get("PASSWORD"));
+        loginPage.loginRISE(data.get("EMAIL"), data.get("PASSWORD"));
         loginPage.verifyLoginSuccess();
     }
 
     @Test(priority = 2, dataProvider = "data_login_excel_email_invalid", dataProviderClass = DataProviderFactory.class)
-    public void testLoginFailedWithEmailInvalid(Hashtable < String, String > data) {
+    public void testLoginFailedWithEmailInvalid(Hashtable<String, String> data) {
         loginPage = new LoginPage();
-        loginPage.loginCRM(data.get("EMAIL"), data.get("PASSWORD"));
+        loginPage.loginRISE(data.get("EMAIL"), data.get("PASSWORD"));
         loginPage.verifyLoginFailedWithEmailOrPasswordInvalid();
     }
 
     @Test(priority = 3, dataProvider = "data_login_excel_password_invalid", dataProviderClass = DataProviderFactory.class)
-    public void testLoginFailedWithPasswordInvalid(Hashtable < String, String > data) {
+    public void testLoginFailedWithPasswordInvalid(Hashtable<String, String> data) {
         loginPage = new LoginPage();
-        loginPage.loginCRM(data.get("EMAIL"), data.get("PASSWORD"));
+        loginPage.loginRISE(data.get("EMAIL"), data.get("PASSWORD"));
         loginPage.verifyLoginFailedWithEmailOrPasswordInvalid();
+    }
+
+    @Test(priority = 4, dataProvider = "data_login_excel_email_require", dataProviderClass = DataProviderFactory.class)
+    public void testLoginFailedWithEmailRequire(Hashtable<String, String> data) {
+        loginPage = new LoginPage();
+        loginPage.loginRISE(data.get("EMAIL"), data.get("PASSWORD"));
+        loginPage.verifyLoginFailedWithEmailRequire();
+    }
+
+    @Test(priority = 5, dataProvider = "data_login_excel_password_require", dataProviderClass = DataProviderFactory.class)
+    public void testLoginFailedWithPasswordRequire(Hashtable<String, String> data) {
+        loginPage = new LoginPage();
+        loginPage.loginRISE(data.get("EMAIL"), data.get("PASSWORD"));
+        loginPage.verifyLoginFailedWithPasswordRequire();
     }
 }
