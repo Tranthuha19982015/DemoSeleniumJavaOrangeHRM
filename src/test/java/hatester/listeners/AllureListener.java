6312ff64df8dc7258ab04dc7982762a83081033d
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-17 11:51:23 +0700

    thêm class AllureListener và services để hỗ trợ các phiên bản sau TestNg 7.4.0

---
+++ src/test/java/hatester/listeners/AllureListener.java
@@ -0,0 +1,72 @@
+package hatester.listeners;
+
+import hatester.drivers.DriverManager;
+import hatester.helpers.PropertiesHelper;
+import io.qameta.allure.Allure;
+import io.qameta.allure.listener.TestLifecycleListener;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.TestResult;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
+
+public class AllureListener implements TestLifecycleListener {
+
+    @Override
+    public void beforeTestSchedule(TestResult result) {
+    }
+
+    @Override
+    public void afterTestSchedule(TestResult result) {
+    }
+
+    @Override
+    public void beforeTestUpdate(TestResult result) {
+    }
+
+    @Override
+    public void afterTestUpdate(TestResult result) {
+    }
+
+    @Override
+    public void beforeTestStart(TestResult result) {
+    }
+
+    @Override
+    public void afterTestStart(TestResult result) {
+    }
+
+    @Override
+    public void beforeTestStop(TestResult result) {
+        if (PropertiesHelper.getValue("SCREENSHOT_SUCCESS").equals("true")) {
+            if (result.getStatus().equals(Status.PASSED)) {
+                if (DriverManager.getDriver() != null) {
+                    Allure.addAttachment(result.getName() + "_Passed_Screenshot",
+                            new ByteArrayInputStream(((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BYTES)));
+                }
+            }
+        }
+        if (PropertiesHelper.getValue("SCREENSHOT_FAILURE").equals("true")) {
+            if (result.getStatus().equals(Status.FAILED)) {
+                if (DriverManager.getDriver() != null) {
+                    Allure.addAttachment(result.getName() + "_Failed_Screenshot",
+                            new ByteArrayInputStream(((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BYTES)));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void afterTestStop(TestResult result) {
+    }
+
+    @Override
+    public void beforeTestWrite(TestResult result) {
+    }
+
+    @Override
+    public void afterTestWrite(TestResult result) {
+    }
+
+}
