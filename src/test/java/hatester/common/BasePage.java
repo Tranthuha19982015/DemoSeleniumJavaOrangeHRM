163eafe82210fb7ee60792f05d48b3e7aa94564c
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-18 17:37:38 +0700

    add click menu Dashboard and Clients

--- src/test/java/hatester/common/BasePage.java
+++ src/test/java/hatester/common/BasePage.java
@@ -1,7 +1,19 @@
 package hatester.common;
 
+import hatester.keywords.WebUI;
+import hatester.pages.clients.ClientsPageCommon;
 import org.openqa.selenium.By;
 
 public class BasePage {
-    public By menuDashboard = By.xpath("//ul[@id='sidebar-menu']/descendant::span[normalize-space()='Dashboard' and @class='menu-text ']");
+    public By menuDashboard = By.xpath("//ul[@id='sidebar-menu']/descendant::span[normalize-space()='Dashboard' and contains(@class,'menu-text')]");
+    public By menuClients = By.xpath("//ul[@id='sidebar-menu']/descendant::span[normalize-space()='Clients' and contains(@class,'menu-text')]");
+
+    public void clickMenuDashboard() {
+        WebUI.clickToElement(menuDashboard);
+    }
+
+    public ClientsPageCommon clickMenuClients() {
+        WebUI.clickToElement(menuClients);
+        return new ClientsPageCommon();
+    }
 }
