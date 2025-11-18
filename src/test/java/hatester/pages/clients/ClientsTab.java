db616317ad4b254552003ff9564c5777df874a9b
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-18 18:10:18 +0700

    update code add new client

--- src/test/java/hatester/pages/clients/ClientsTab.java
+++ src/test/java/hatester/pages/clients/ClientsTab.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
 
-public class ClientsTab extends ClientsPageCommon{
+import java.util.Hashtable;
+
+public class ClientsTab extends ClientsPageCommon {
     //Clients List Page
     private By inputSearchClients = By.xpath("//div[@id='client-table_filter']/descendant::input[@type='search']");
 
@@ -31,6 +33,7 @@ public class ClientsTab extends ClientsPageCommon{
     private By radioButtonTypePerson = By.xpath("//input[@id='type_organization']");
     //input
     private By inputCompanyName = By.xpath("//input[@id='company_name']");
+    private By errorMessageRequireName = By.xpath("//span[@id='company_name-error']");
     //dropdown Owner
     private By dropdownOwner = By.xpath("(//label[normalize-space()='Owner' and @for='created_by']/following-sibling::div)/descendant::a");
     private By inputSearchOwner = By.xpath("//input[@id='created_by']/following::label[normalize-space()='Owner']/parent::div");
@@ -43,9 +46,13 @@ public class ClientsTab extends ClientsPageCommon{
     private By inputPhone = By.xpath("//form[@id='client-form']/descendant::input[@id='phone']");
     private By inputWebsite = By.xpath("//form[@id='client-form']/descendant::input[@id='website']");
     private By inputVATNumber = By.xpath("//form[@id='client-form']/descendant::input[@id='vat_number']");
-    private By inputGTSNumber = By.xpath("//form[@id='client-form']/descendant::input[@id='gst_number']");
+    private By inputGSTNumber = By.xpath("//form[@id='client-form']/descendant::input[@id='gst_number']");
     private By inputClientsGroups = By.xpath("((//label[@for='groups']/following-sibling::div)/descendant::ul)/descendant::input");
     private By inputLabel = By.xpath("((//label[@for='client_labels']/following-sibling::div)/descendant::ul)/descendant::input");
+    //button
+    private By buttonClose = By.xpath("//button[@id='save-and-continue-button']/preceding-sibling::button[normalize-space()='Close']");
+    private By buttonSaveAndContinue = By.xpath("//button[@id='save-and-continue-button']");
+    private By buttonSave = By.xpath("//button[@id='save-and-continue-button']/following-sibling::button[normalize-space()='Save']");
 
 
     public void clickButtonAddClient() {
@@ -56,15 +63,47 @@ public class ClientsTab extends ClientsPageCommon{
         Assert.assertTrue(WebUI.checkElementExist(headerAddClient, 10), "The header Add Client is not display!");
     }
 
-    public void fillDataNewClient(String name, String owner,String address, int flag) {
-        if (flag == 1) {
+    public void fillDataNewClient(Hashtable<String, String> data) {
+        if (data.get("TYPE").equalsIgnoreCase("Person")) {
             WebUI.clickToElement(radioButtonTypePerson);
+        } else {
+            WebUI.clickToElement(radioButtonTypeOrganization);
         }
-        WebUI.setText(inputCompanyName, name);
+        WebUI.setText(inputCompanyName, data.get("COMPANY_NAME"));
 
         WebUI.clickToElement(dropdownOwner);
-        WebUI.setTextAndKey(inputSearchOwner, owner, Keys.ENTER);
+        WebUI.setTextAndKey(inputSearchOwner, data.get("OWNER"), Keys.ENTER);
 
-        WebUI.setText(inputAddress,address);
+        WebUI.setText(inputAddress, data.get("ADDRESS"));
+        WebUI.setText(inputCity, data.get("CITY"));
+        WebUI.setText(inputState, data.get("STATE"));
+        WebUI.setText(inputZip, data.get("ZIP"));
+        WebUI.setText(inputCountry, data.get("COUNTRY"));
+        WebUI.setText(inputPhone, data.get("PHONE"));
+        WebUI.setText(inputWebsite, data.get("WEBSITE"));
+        WebUI.setText(inputVATNumber, data.get("VAT"));
+        WebUI.setText(inputGSTNumber, data.get("GST"));
+        WebUI.setTextAndKey(inputClientsGroups, data.get("CLIENT_GROUPS"), Keys.ENTER);
+        WebUI.setTextAndKey(inputLabel, data.get("LABELS"), Keys.ENTER);
+    }
+
+    public void clickButtonSave() {
+        WebUI.clickToElement(buttonSave);
+    }
+
+    public void clickButtonSaveAndContinue() {
+        WebUI.clickToElement(buttonSaveAndContinue);
+    }
+
+    public void verifyClientNameRequire(String name) {
+        Assert.assertFalse(WebUI.checkElementExist(errorMessageRequireName, 5), "Client name is null.");
+    }
+
+    public void searchAndCheckNewClient(String name) {
+        WebUI.waitForPageLoaded();
+        WebUI.clickToElement(inputSearchClients);
+        WebUI.setTextAndKey(inputSearchClients, name, Keys.ENTER);
+        WebUI.waitForPageLoaded();
+        Assert.assertTrue(WebUI.checkElementExist(firstRowNameClients(name)), "The client was not added correctly just now.");
     }
 }
